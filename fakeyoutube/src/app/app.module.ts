8f017e60b2c18a188dd117f2398ab1e6bb9eaa96
Daniel Malo <danielmaloosorio@gmail.com>
2022-03-10 18:42:26 -0600

    Added bootstrap and created basecomponent
    Bootstrap for simpler styling is been added.
    
    First component on which to build the app is created and updated with the content from index.html from preivous commits.

--- fakeyoutube/src/app/app.module.ts
+++ fakeyoutube/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasecomponentComponent } from './basecomponent/basecomponent.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasecomponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
